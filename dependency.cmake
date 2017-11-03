d46a09c1df5196972439fc798e0dd3d1c12b4753
David Callu <callu.david@gmail.com>
2017-11-03 23:24:50 +0100

    feat(cmake): add version 0.6.0-1

---
+++ dependency.cmake
@@ -0,0 +1,3 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+#byd__package__set_dependency(${package})
---
+++ id.cmake
@@ -0,0 +1,14 @@
+set(package libwebp)
+set(version 0.6.0)
+
+byd__package__info(${package}
+    MAINTAINER_NAME "David Callu"
+    MAINTAINER_EMAIL "callu.david@gmail.com"
+    VERSION ${version}-1
+    ABI ${version}
+    )
+
+byd__package__download_info(${package}
+    URL "https://github.com/webmproject/libwebp/archive/v0.6.0.tar.gz"
+    URL_HASH SHA1=7294e5571657b8acdc3048d0a2d7a4cf746f5c80
+)
