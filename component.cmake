2490455419d2852bbaad51ab2dabb95793199c79
David Callu <callu.david@gmail.com>
2017-11-13 23:03:45 +0100

    feat(cmake): add support to per component build

--- component.cmake
+++ component.cmake
@@ -2,7 +2,8 @@ include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
 
 
 
-byd__package__add_component(${package} libwepmux)
+byd__package__add_component(${package} libwebp)
+byd__package__add_component(${package} libwebpmux)
 byd__package__add_component(${package} libwebpdemux)
 byd__package__add_component(${package} libwebpdecoder)
 byd__package__add_component(${package} libwebpextras)
