47258794ed67583b230885c43c8b1ca8c6b7ea9f
David Callu <callu.david@gmail.com>
2018-04-01 00:19:30 +0200

    chore(version): bup to 0.6.0-3

--- id.cmake
+++ id.cmake
@@ -4,7 +4,7 @@ set(version 0.6.0)
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-1
+    VERSION ${version}-3
     ABI ${version}
     )
 
